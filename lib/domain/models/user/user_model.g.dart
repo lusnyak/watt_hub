b36225c98c70c93922544d5c2c59a2bc98234555
Jora <nersisyanjor3@gamil.com>
2024-09-24 14:09:27 +0400

    We created the user model using the freezed package.

---
+++ lib/domain/models/user/user_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserModelImpl _$$UserModelImplFromJson(Map<String, dynamic> json) =>
+    _$UserModelImpl(
+      id: (json['id'] as num).toInt(),
+      name: json['name'] as String,
+      age: (json['age'] as num).toInt(),
+    );
+
+Map<String, dynamic> _$$UserModelImplToJson(_$UserModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'age': instance.age,
+    };
