9a2e0552851def24705fe609bd5eb7ce9f6186bd
Lusine Khachatryan <luskhach3108@gmail.com>
2024-10-04 10:26:57 +0400

    fix text styles names

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # watt_hub
+
+## Generate codes
+
+fvm flutter pub run build_runner build --delete-conflicting-outputs
+fvm flutter packages pub run build_runner build --delete-conflicting-outputs
+
+## WattHub UIKit codes generates commands:
+cd packages/watt_hub_uikit && fvm flutter pub get && fvm flutter packages pub run build_runner build --delete-conflicting-outputs
+
+## WattHub localizations codes generates commands:
+cd packages/watt_hub_localization && fvm flutter pub get && fvm flutter gen-l10n
+
+ 
--- packages/watt_hub_uikit/lib/src/theme/watt_hub_text_styles.dart
+++ packages/watt_hub_uikit/lib/src/theme/watt_hub_text_styles.dart
@@ -7,127 +7,17 @@ TextStyle get exampleTextStyle => TextStyle(
       fontFamily: FontFamily.urbanist,
       fontWeight: FontWeight.w600,
     );
-TextStyle get urbanist16RegularTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w400,
-  height: 25.6 / 16.0,
-  fontSize: 16.sp,
-  color: WattHubColors.textColor,
-);
-TextStyle get urbanist16MediumTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w500,
-  height: 25.6 / 16.0,
-  fontSize: 16.sp,
-  color: WattHubColors.textColor,
-);
 
-TextStyle get urbanist16SemiboldTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w600,
-  height: 25.6 / 16.0,
-  fontSize: 16.sp,
-  color: WattHubColors.textColor,
-);
-
-TextStyle get urbanist18RegularTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w400,
-  height: 28.8 / 18.0,
-  fontSize: 18.sp,
-  color: WattHubColors.textColor,
-);
-TextStyle get urbanist18MediumTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w500,
-  height: 28.8 / 18.0,
-  fontSize: 18.sp,
-  color: WattHubColors.textColor,
-);
-
-TextStyle get urbanist18SemiboldTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w600,
-  height: 28.8 / 18.0,
-  fontSize: 18.sp,
-  color: WattHubColors.textColor,
-);
-
-TextStyle get urbanist24RegularTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w400,
-  height: 38.4 / 24.0,
-  fontSize: 24.sp,
-  color: WattHubColors.textColor,
-);
-TextStyle get urbanist24MediumTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w500,
-  height: 38.4 / 24.0,
-  fontSize: 24.sp,
-  color: WattHubColors.textColor,
-);
-
-TextStyle get urbanist24SemiboldTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w600,
-  height: 38.4 / 24.0,
-  fontSize: 24.sp,
-  color: WattHubColors.textColor,
-);
-TextStyle get urbanist32RegularTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w400,
-  height: 51.2 / 32.0,
-  fontSize: 32.sp,
-  color: WattHubColors.textColor,
-);
-TextStyle get urbanist32MediumTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w500,
-  height: 51.2 / 32.0,
-  fontSize: 32.sp,
-  color: WattHubColors.textColor,
-);
-
-TextStyle get urbanist32SemiboldTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w600,
-  height: 51.2 / 32.0,
-  fontSize: 32.sp,
-  color: WattHubColors.textColor,
-);
-TextStyle get urbanist48RegularTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w400,
-  height: 76.8 / 48.0,
-  fontSize: 48.sp,
-  color: WattHubColors.textColor,
-);
-TextStyle get urbanist48MediumTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w500,
-  height: 76.8 / 48.0,
-  fontSize: 48.sp,
-  color: WattHubColors.textColor,
-);
-
-TextStyle get urbanist48SemiboldTextStyle => TextStyle(
-  fontFamily: FontFamily.urbanist,
-  fontWeight: FontWeight.w600,
-  height: 76.8 / 48.0,
-  fontSize: 48.sp,
-  color: WattHubColors.textColor,
-);
-
-TextStyle get urbanist14RegularTextStyle => TextStyle(
+/// 14 size
+TextStyle get body14RegularTextStyle => TextStyle(
   fontFamily: FontFamily.urbanist,
   fontWeight: FontWeight.w400,
   height: 20.0 / 14.0,
   fontSize: 14.sp,
   color: WattHubColors.textColor,
 );
-TextStyle get urbanist14MediumTextStyle => TextStyle(
+
+TextStyle get body14MediumTextStyle => TextStyle(
   fontFamily: FontFamily.urbanist,
   fontWeight: FontWeight.w500,
   height: 20.0 / 14.0,
@@ -135,11 +25,137 @@ TextStyle get urbanist14MediumTextStyle => TextStyle(
   color: WattHubColors.textColor,
 );
 
-TextStyle get urbanist14SemiboldTextStyle => TextStyle(
+TextStyle get body14SemiBoldTextStyle => TextStyle(
   fontFamily: FontFamily.urbanist,
   fontWeight: FontWeight.w600,
   height: 20.0 / 14.0,
   fontSize: 14.sp,
   color: WattHubColors.textColor,
 );
+
+/// 16 size
+TextStyle get body16RegularTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w400,
+  height: 25.6 / 16.0,
+  fontSize: 16.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body16MediumTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w500,
+  height: 25.6 / 16.0,
+  fontSize: 16.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body16SemiBoldTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w600,
+  height: 25.6 / 16.0,
+  fontSize: 16.sp,
+  color: WattHubColors.textColor,
+);
+
+/// 18 size
+TextStyle get body18RegularTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w400,
+  height: 28.8 / 18.0,
+  fontSize: 18.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body18MediumTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w500,
+  height: 28.8 / 18.0,
+  fontSize: 18.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body18SemiBoldTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w600,
+  height: 28.8 / 18.0,
+  fontSize: 18.sp,
+  color: WattHubColors.textColor,
+);
+
+/// 24 size
+TextStyle get body24RegularTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w400,
+  height: 38.4 / 24.0,
+  fontSize: 24.sp,
+  color: WattHubColors.textColor,
+);
+TextStyle get body24MediumTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w500,
+  height: 38.4 / 24.0,
+  fontSize: 24.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body24SemiBoldTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w600,
+  height: 38.4 / 24.0,
+  fontSize: 24.sp,
+  color: WattHubColors.textColor,
+);
+
+/// 32 size
+TextStyle get body32RegularTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w400,
+  height: 51.2 / 32.0,
+  fontSize: 32.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body32MediumTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w500,
+  height: 51.2 / 32.0,
+  fontSize: 32.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body32SemiBoldTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w600,
+  height: 51.2 / 32.0,
+  fontSize: 32.sp,
+  color: WattHubColors.textColor,
+);
+
+/// 48 size
+TextStyle get body48RegularTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w400,
+  height: 76.8 / 48.0,
+  fontSize: 48.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body48MediumTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w500,
+  height: 76.8 / 48.0,
+  fontSize: 48.sp,
+  color: WattHubColors.textColor,
+);
+
+TextStyle get body48SemiBoldTextStyle => TextStyle(
+  fontFamily: FontFamily.urbanist,
+  fontWeight: FontWeight.w600,
+  height: 76.8 / 48.0,
+  fontSize: 48.sp,
+  color: WattHubColors.textColor,
+);
+
+
 
