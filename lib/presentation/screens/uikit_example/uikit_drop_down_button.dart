4209f37e25a2321a8065f6fce0a31e26512b83ac
Nikolay Nikoghosyan <19nikonikogh88gmail.com>
2024-10-07 17:00:00 +0400

    fixed errors in drop_down_button

--- lib/presentation/screens/uikit_example/uikit_drop_down_button.dart
+++ lib/presentation/screens/uikit_example/uikit_drop_down_button.dart
@@ -34,7 +34,8 @@ class _UikitDropDownButtonState extends State<UikitDropDownButton> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Input or Select Station Address'),
+        titleTextStyle: body16RegularTextStyle,
+        title: const Text('Dropdown 2'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -42,82 +43,11 @@ class _UikitDropDownButtonState extends State<UikitDropDownButton> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             WhDropDownButton<String>(
-              items: ["Zora", "Nikolay", "Nune"],
+              items: streets,
               itemLabel: (String el) => el,
+              onChanged: (value) => debugPrint(value),
+              suffixText: 'AM',
             )
-            // // Switch to toggle between dropdown and text input
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //   children: [
-            //     const Text('Use Input Station Address?'),
-            //     Switch(
-            //       value: isManualInput,
-            //       onChanged: (value) {
-            //         setState(() {
-            //           isManualInput = value;
-            //           if (!isManualInput) {
-            //             streetController
-            //                 .clear(); // Clear input if switching back to dropdown
-            //           } else {
-            //             selectedStreet =
-            //                 null; // Clear dropdown selection if switching to manual input
-            //           }
-            //         });
-            //       },
-            //     ),
-            //   ],
-            // ),
-            // const SizedBox(height: 20),
-            // // Show DropdownButton2 if manual input is disabled, else show TextFormField
-            // if (!isManualInput)
-            //   DropdownButton2(
-            //     iconStyleData: const IconStyleData(
-            //       icon: Icon(
-            //         Icons.keyboard_arrow_down,
-            //       ),
-            //       iconSize: 20,
-            //       iconEnabledColor: WattHubColors.darkMoodColor,
-            //       iconDisabledColor: WattHubColors.darkMoodColor,
-            //     ),
-            //     isExpanded: true,
-            //     hint: Text(
-            //       'Select Street',
-            //       style: body16RegularTextStyle,
-            //     ),
-            //     items: streets
-            //         .map((street) => DropdownMenuItem<String>(
-            //               value: street,
-            //               child: Text(
-            //                 street,
-            //                 style: body16RegularTextStyle,
-            //               ),
-            //             ))
-            //         .toList(),
-            //     value: selectedStreet,
-            //     onChanged: (value) {
-            //       setState(() {
-            //         selectedStreet = value;
-            //       });
-            //     },
-            //   )
-            // else
-            //   TextFormField(
-            //     controller: streetController,
-            //     onTapOutside: (_) =>
-            //         FocusManager.instance.primaryFocus?.unfocus(),
-            //     decoration: InputDecoration(
-            //       labelText: 'Enter Station Address',
-            //       border: OutlineInputBorder(
-            //         borderRadius: BorderRadius.circular(10),
-            //       ),
-            //     ),
-            //   ),
-            // const SizedBox(height: 20),
-            // // Display the selected or inputted street
-            // Text(
-            //   'Selected/Inputted Street: ${isManualInput ? streetController.text : selectedStreet ?? 'None'}',
-            //   style: body18RegularTextStyle,
-            // ),
           ],
         ),
       ),
