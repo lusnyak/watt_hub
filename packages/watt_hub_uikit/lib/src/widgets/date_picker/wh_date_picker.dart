e3966850adadf0bf4cfa26a7921cfc6ff6b90d3e
Jora <nersisyanjor3@gamil.com>
2024-10-08 14:54:06 +0400

    Added dates logics.

--- packages/watt_hub_uikit/lib/src/widgets/date_picker/wh_date_picker.dart
+++ packages/watt_hub_uikit/lib/src/widgets/date_picker/wh_date_picker.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:watt_hub_uikit/utils/extensions/wh_date_picker_extension.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
-class WHDatePicker extends StatelessWidget {
-  const WHDatePicker({
-    super.key,
-    this.date,
-    required this.onDateTimeChanged,
-    required this.mode,
-    this.time,
-    this.title,
-  });
+class WhDatePicker {
+  final BuildContext context;
 
-  final String? title;
-  final DateTime? date;
-  final DateTime? time;
-  final ValueChanged<DateTime> onDateTimeChanged;
-  final CupertinoDatePickerMode mode;
+  WhDatePicker._(this.context);
 
-  void _showDialog(context, child) {
-    showCupertinoModalPopup<void>(
+  factory WhDatePicker.of(BuildContext cont) => WhDatePicker._(cont);
+
+  Future<TimeOfDay?> showTimePicker({
+    TimeOfDay? initialTime,
+  }) async {
+    return await showCupertinoModalPopup<TimeOfDay?>(
       context: context,
       builder: (BuildContext context) => Container(
-        height: 216,
         padding: paddingOnlyTop6,
         margin: EdgeInsets.only(
           bottom: MediaQuery.of(context).viewInsets.bottom,
@@ -30,40 +23,106 @@ class WHDatePicker extends StatelessWidget {
         color: WattHubColors.whiteColor,
         child: SafeArea(
           top: false,
-          child: child,
+          child: _WHDatePicker(
+            time: initialTime,
+            mode: CupertinoDatePickerMode.time,
+          ),
         ),
       ),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        ElevatedButton(
-          onPressed: () => _showDialog(
-            context,
-            CupertinoDatePicker(
-              initialDateTime: date ?? time,
-              mode: mode,
-              use24hFormat: true,
-              showDayOfWeek: true,
-              onDateTimeChanged: onDateTimeChanged,
-            ),
-          ),
-          child: Text(
-            title ?? '',
-            textAlign: TextAlign.center,
+  Future<DateTime?> showDatePicker({
+    DateTime? initialDate,
+  }) async {
+    return await showCupertinoModalPopup<DateTime?>(
+      context: context,
+      builder: (BuildContext context) => Container(
+        padding: paddingOnlyTop6,
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        color: WattHubColors.whiteColor,
+        child: SafeArea(
+          top: false,
+          child: _WHDatePicker(
+            date: initialDate,
+            mode: CupertinoDatePickerMode.date,
           ),
         ),
-        40.widthBox,
-        Text(
-          date != null
-              ? '${date?.month}-${date?.day}-${date?.year}'
-              : '${time?.hour}:${time?.minute}',
-          textAlign: TextAlign.center,
-          style: body18MediumTextStyle,
+      ),
+    );
+  }
+}
+
+class _WHDatePicker extends StatefulWidget {
+  const _WHDatePicker({
+    required this.mode,
+    this.time,
+    this.date,
+  });
+
+  final DateTime? date;
+  final TimeOfDay? time;
+  final CupertinoDatePickerMode mode;
+
+  @override
+  State<_WHDatePicker> createState() => _WHDatePickerState();
+}
+
+class _WHDatePickerState extends State<_WHDatePicker> {
+  DateTime? choosedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.mode == CupertinoDatePickerMode.time) {
+      choosedDate = widget.time?.toDate();
+    } else {
+      choosedDate = widget.date;
+    }
+  }
+
+  void onDateTimeChanged(DateTime date) {
+    choosedDate = date;
+  }
+
+  void onCancelled() {
+    Navigator.of(context).pop(null);
+  }
+
+  void onApplied() {
+    if (widget.mode == CupertinoDatePickerMode.time) {
+      Navigator.of(context).pop(
+          choosedDate != null ? TimeOfDay.fromDateTime(choosedDate!) : null);
+    } else {
+      Navigator.of(context).pop(choosedDate);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(onPressed: onCancelled, child: const Text("Cancel")),
+            TextButton(onPressed: onApplied, child: const Text("Apply")),
+          ],
+        ),
+        SizedBox(
+          height: 200,
+          child: CupertinoDatePicker(
+            initialDateTime: choosedDate,
+            mode: widget.mode,
+            use24hFormat: true,
+            showDayOfWeek: true,
+            onDateTimeChanged: onDateTimeChanged,
+          ),
         ),
       ],
     );
--- packages/watt_hub_uikit/lib/watt_hub_uikit.dart
+++ packages/watt_hub_uikit/lib/watt_hub_uikit.dart
@@ -3,6 +3,7 @@ library watt_hub_uikit;
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:awesome_extensions/awesome_extensions.dart';
 export 'package:cupertino_icons/cupertino_icons.dart';
+export 'package:intl/intl.dart';
 
 export 'src/widgets/widgets.dart';
 export 'src/assets/assets.dart';
