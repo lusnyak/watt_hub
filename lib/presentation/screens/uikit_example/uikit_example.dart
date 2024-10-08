d9d1a6a7f6572ebca0c32f35a31cc89d57964fc3
Lusine Khachatryan <luskhach3108@gmail.com>
2024-10-08 13:07:26 +0400

    rebase fix conflict 1

--- lib/presentation/screens/uikit_example/uikit_example.dart
+++ lib/presentation/screens/uikit_example/uikit_example.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:watt_hub/presentation/screens/uikit_example/uikit_text_field.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
+import 'package:watt_hub/presentation/screens/uikit_example/uikit_drop_down_button.dart';
 
 @RoutePage()
 class UikitExamplePage extends StatelessWidget {
@@ -9,9 +9,9 @@ class UikitExamplePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+     return Scaffold(
       body: const SafeArea(
-        child: UikitTextField(),
+        child: UikitDropDownButton(),
       ).paddingAll(20.0),
     );
   }
