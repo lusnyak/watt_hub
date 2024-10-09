0493a6d388864fe45e305e4caffb4a3ca0a18d57
Jora <nersisyanjor3@gamil.com>
2024-10-09 17:49:24 +0400

    Added cluster logic for map.

--- packages/watt_hub_uikit/lib/src/theme/watt_hub_main_theme.dart
+++ packages/watt_hub_uikit/lib/src/theme/watt_hub_main_theme.dart
@@ -3,13 +3,15 @@ import 'package:watt_hub_uikit/src/theme/watt_input_decorations.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
 ThemeData get lightTheme => ThemeData(
-      colorScheme: const ColorScheme.light(
-          primary: WattHubColors.primaryGreenColor,
-          secondary: WattHubColors.primaryLightGreenColor),
-      primaryColor: WattHubColors.primaryGreenColor,
-      elevatedButtonTheme: elevatedButtonStyle,
-      inputDecorationTheme: inputDecorationTheme,
-    );
+    colorScheme: const ColorScheme.light(
+        primary: WattHubColors.primaryGreenColor,
+        secondary: WattHubColors.primaryLightGreenColor),
+    primaryColor: WattHubColors.primaryGreenColor,
+    elevatedButtonTheme: elevatedButtonStyle,
+    inputDecorationTheme: inputDecorationTheme,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.transparent,
+    ));
 
 /// TODO: - dark theme property
 
