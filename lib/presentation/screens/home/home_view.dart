643573d44823926e6ce6d0eb7dcbe160498ad79d
Jora <nersisyanjor3@gamil.com>
2024-10-09 17:49:24 +0400

    Added cluster logic for map.

--- lib/presentation/screens/home/home_view.dart
+++ lib/presentation/screens/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:watt_hub/config/routes/app_router.dart';
@@ -11,6 +12,7 @@ import 'package:watt_hub/presentation/screens/home/charging_station_bloc/chargin
 import 'package:watt_hub/presentation/screens/home/charging_station_bloc/charging_station_state.dart';
 import 'package:watt_hub/presentation/screens/home/widgets/stations_list.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -41,12 +43,12 @@ class _HomeViewState extends State<HomeView> {
     }
   }
 
-  void show(ChargingStationModel station) {
+  void showStationInfo(ChargingStationModel station) {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
         return Container(
-          height: 300,
+          height: 220,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -65,16 +67,52 @@ class _HomeViewState extends State<HomeView> {
                 ],
               ),
               Row(
+                children: [
+                  Text(
+                    '${station.averageRate}',
+                    style: body14RegularTextStyle,
+                  ),
+                  10.widthBox,
+                  RatingBarIndicator(
+                    rating: station.averageRate,
+                    itemBuilder: (
+                      context,
+                      index,
+                    ) =>
+                        const Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    itemCount: 5,
+                    itemSize: 25.0,
+                    direction: Axis.horizontal,
+                  ),
+                  10.widthBox,
+                  Text(
+                    '(${station.reviews.length} reviews)',
+                    style: body14RegularTextStyle.copyWith(
+                      color: Colors.grey,
+                    ),
+                  )
+                ],
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  WHOutlinedButton(
-                    title: 'View',
-                    onPressed: () {},
+                  Expanded(
+                    child: WHOutlinedButton(
+                      title: 'View',
+                      onPressed: () =>
+                          AutoRouter.of(context).push(const StationInfoRoute()),
+                    ),
                   ),
                   20.widthBox,
-                  WHElevatedButton.primary(
-                    title: 'Book',
-                    onPressed: () {},
+                  Expanded(
+                    child: WHElevatedButton.primary(
+                      title: 'Book',
+                      onPressed: () =>
+                          AutoRouter.of(context).push(const DetailRoute()),
+                    ),
                   )
                 ],
               )
@@ -86,60 +124,80 @@ class _HomeViewState extends State<HomeView> {
   }
 
   Widget _mapContainer(List<ChargingStationModel> chargingStations) {
-    return Container(
-      child: currentLocation != null
-          ? FlutterMap(
-              mapController: mapController,
-              options: MapOptions(
-                initialCenter:
-                    currentLocation ?? const LatLng(40.7942, 43.84528),
-                initialZoom: 18.0,
-                onMapReady: () {
-                  setState(() {
-                    isMapReady = true;
-                  });
-                },
+    return currentLocation != null
+        ? FlutterMap(
+            mapController: mapController,
+            options: MapOptions(
+              initialCenter: currentLocation ?? const LatLng(40.7942, 43.84528),
+              initialZoom: 18.0,
+              onMapReady: () {
+                setState(() {
+                  isMapReady = true;
+                });
+              },
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
               ),
-              children: [
-                TileLayer(
-                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  userAgentPackageName: 'com.example.app',
-                ),
-                if (isMapReady)
-                  MarkerLayer(markers: [
-                    for (var item in chargingStations)
-                      Marker(
-                        width: 56.0,
-                        height: 56.0,
-                        point: LatLng(item.latitude, item.longitude),
-                        child: GestureDetector(
-                          onTap: () => {
-                            show(item),
-                          },
-                          child: const Icon(
-                            Icons.charging_station,
-                            size: 56.0,
-                            color: WattHubColors.primaryGreenColor,
+              if (isMapReady)
+                MarkerClusterLayerWidget(
+                  options: MarkerClusterLayerOptions(
+                    maxClusterRadius: 45,
+                    size: const Size(40, 40),
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(50),
+                    maxZoom: 15,
+                    markers: [
+                      for (var item in chargingStations)
+                        Marker(
+                          width: 56.0,
+                          height: 56.0,
+                          point: LatLng(item.latitude, item.longitude),
+                          child: GestureDetector(
+                            onTap: () => {
+                              showStationInfo(item),
+                            },
+                            child: const Icon(
+                              Icons.charging_station,
+                              size: 56.0,
+                              color: WattHubColors.primaryGreenColor,
+                            ),
                           ),
                         ),
-                      ),
-                    Marker(
-                      width: 80.0,
-                      height: 80.0,
-                      point: currentLocation ?? const LatLng(40.7942, 43.84528),
-                      child: const Icon(
-                        Icons.location_on_outlined,
-                        size: 56.0,
-                        color: WattHubColors.primaryGreenColor,
-                      ),
-                    )
-                  ])
-              ],
-            )
-          : const Center(
-              child: CircularProgressIndicator(),
-            ),
-    );
+                      Marker(
+                        width: 80.0,
+                        height: 80.0,
+                        point:
+                            currentLocation ?? const LatLng(40.7942, 43.84528),
+                        child: const Icon(
+                          Icons.location_on_outlined,
+                          size: 56.0,
+                          color: WattHubColors.primaryGreenColor,
+                        ),
+                      )
+                    ],
+                    builder: (context, markers) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.blue),
+                        child: Center(
+                          child: Text(
+                            markers.length.toString(),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+            ],
+          )
+        : const Center(
+            child: CircularProgressIndicator(),
+          );
   }
 
   @override
@@ -152,10 +210,12 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.transparent,
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         leading: nil,
         backgroundColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
         elevation: 0,
         actions: [
           WHIconButton.secondary(
@@ -200,6 +260,7 @@ class _HomeViewState extends State<HomeView> {
         children: [
           if (isList)
             FloatingActionButton(
+              shape: const StadiumBorder(),
               onPressed: () {
                 if (currentLocation != null) {
                   mapController.move(currentLocation!, 18.0);
@@ -209,6 +270,7 @@ class _HomeViewState extends State<HomeView> {
             ),
           20.widthBox,
           FloatingActionButton(
+            shape: const StadiumBorder(),
             onPressed: () {
               setState(() {
                 isList = !isList;
--- packages/watt_hub_uikit/lib/src/theme/watt_hub_main_theme.dart
+++ packages/watt_hub_uikit/lib/src/theme/watt_hub_main_theme.dart
@@ -4,15 +4,18 @@ import 'package:watt_hub_uikit/src/theme/watt_input_decorations.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
 ThemeData get lightTheme => ThemeData(
-    textTheme: GoogleFonts.urbanistTextTheme(),
-    colorScheme: const ColorScheme.light(
-      primary: WattHubColors.primaryGreenColor,
-      secondary: WattHubColors.primaryLightGreenColor,
-    ),
-    primaryColor: WattHubColors.primaryGreenColor,
-    elevatedButtonTheme: elevatedButtonStyle,
-    inputDecorationTheme: inputDecorationTheme,
-    dividerTheme: const DividerThemeData(color: WattHubColors.grayColor));
+      textTheme: GoogleFonts.urbanistTextTheme(),
+      colorScheme: const ColorScheme.light(
+        primary: WattHubColors.primaryGreenColor,
+        secondary: WattHubColors.primaryLightGreenColor,
+      ),
+      primaryColor: WattHubColors.primaryGreenColor,
+      elevatedButtonTheme: elevatedButtonStyle,
+      inputDecorationTheme: inputDecorationTheme,
+      dividerTheme: const DividerThemeData(
+        color: WattHubColors.grayColor,
+      ),
+    );
 
 /// TODO: - dark theme property
 
