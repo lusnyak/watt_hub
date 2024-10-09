77ef9088258ea1703c488feda0f821ad16210dad
Nikolay Nikoghosyan <19nikonikogh88gmail.com>
2024-10-09 16:27:09 +0400

    Changed resent parameter in verification page and aded continue button.

--- lib/presentation/screens/verification/verification_screen.dart
+++ lib/presentation/screens/verification/verification_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:watt_hub/config/routes/app_router.dart';
 import 'package:watt_hub_localization/watt_hub_localization.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
@@ -12,40 +13,51 @@ class VerificationScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Row(
-              children: [
-                Text(
-                  AppLocalizations.of(context).otpCodeVerification,
-                  style: body24SemiBoldTextStyle,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    AppLocalizations.of(context).otpCodeVerification,
+                    style: body24SemiBoldTextStyle,
+                  ),
+                  const Text("🔐")
+                ],
+              ),
+              Text(
+                AppLocalizations.of(context).sentAnOtpCode,
+                style: body16RegularTextStyle,
+              ),
+              40.heightBox,
+              const WHPinPut(),
+              30.heightBox,
+              Center(
+                child: Text(
+                  AppLocalizations.of(context).didntReceiveEmail,
+                  style: body14RegularTextStyle,
                 ),
-                const Text("🔐")
-              ],
-            ),
-            Text(
-              AppLocalizations.of(context).sentAnOtpCode,
-              style: body16RegularTextStyle,
-            ),
-            40.heightBox,
-            const WHPinPut(),
-            30.heightBox,
-            Center(
-              child: Text(
-                AppLocalizations.of(context).didntReceiveEmail,
-                style: body14RegularTextStyle,
               ),
-            ),
-            15.heightBox,
-            Center(
-              child: Text(
-                "You can resend code in 48 s",
-                style: body14RegularTextStyle,
+              15.heightBox,
+              Center(
+                child: TextButton(
+                  child: Text(
+                    "resend",
+                    style: body14SemiBoldTextStyle,
+                  ),
+                  onPressed: () {},
+                ),
               ),
-            ),
-          ],
+              WHElevatedButton.primary(
+                title: AppLocalizations.of(context).continueText,
+                onPressed: () => AutoRouter.of(context).replace(
+                  const HomeRoute(),
+                ),
+              ),
+            ],
+          ),
         ),
       ).paddingAll(20.0),
     );
