7ae93f316aaf6982111a8a5c0747398efdce4e5b
Jora <nersisyanjor3@gamil.com>
2024-10-10 15:29:22 +0400

    The home page file has been separated into separate parts.

---
+++ lib/presentation/screens/home/widgets/map_container.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:watt_hub/domain/models/charging_station/charging_station_model.dart';
+import 'package:watt_hub_uikit/watt_hub_uikit.dart';
+import 'station_info_modal.dart';
+
+class MapContainer extends StatelessWidget {
+  final MapController mapController;
+  final LatLng? currentLocation;
+  final bool isMapReady;
+  final List<ChargingStationModel> chargingStations;
+
+  const MapContainer({
+    super.key,
+    required this.mapController,
+    required this.chargingStations,
+    required this.currentLocation,
+    required this.isMapReady,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return currentLocation != null
+        ? FlutterMap(
+            mapController: mapController,
+            options: MapOptions(
+              initialCenter: currentLocation ?? const LatLng(40.7942, 43.84528),
+              initialZoom: 18.0,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+              if (!isMapReady)
+                MarkerClusterLayerWidget(
+                  options: MarkerClusterLayerOptions(
+                    maxClusterRadius: 45,
+                    size: const Size(40, 40),
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(50),
+                    maxZoom: 15,
+                    markers: [
+                      for (var station in chargingStations)
+                        Marker(
+                          width: 56.0,
+                          height: 56.0,
+                          point: LatLng(station.latitude, station.longitude),
+                          child: GestureDetector(
+                            onTap: () => showStationInfo(context, station),
+                            child: const Icon(
+                              Icons.charging_station,
+                              size: 56.0,
+                              color: WattHubColors.primaryGreenColor,
+                            ),
+                          ),
+                        ),
+                      Marker(
+                        width: 80.0,
+                        height: 80.0,
+                        point:
+                            currentLocation ?? const LatLng(40.7942, 43.84528),
+                        child: const Icon(
+                          Icons.location_on_outlined,
+                          size: 56.0,
+                          color: WattHubColors.primaryGreenColor,
+                        ),
+                      )
+                    ],
+                    builder: (context, markers) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: WattHubColors.primaryGreenColor,
+                        ),
+                        child: Center(
+                          child: Text(
+                            markers.length.toString(),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+            ],
+          )
+        : const Center(child: CircularProgressIndicator());
+  }
+}
--- lib/presentation/screens/home/widgets/stations_list.dart
+++ lib/presentation/screens/home/widgets/stations_list.dart
@@ -1,51 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:watt_hub/domain/models/charging_station/charging_station_model.dart';
-import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
 class StationsList extends StatelessWidget {
+  final List<ChargingStationModel> stationsList;
+  final Function(ChargingStationModel) onStationSelected;
+
   const StationsList({
     super.key,
     required this.stationsList,
-    required this.onStationSelected, // Add the callback for station selection
+    required this.onStationSelected,
   });
-
-  final List<ChargingStationModel> stationsList;
-  final Function(ChargingStationModel) onStationSelected; // Callback type
 
   @override
   Widget build(BuildContext context) {
-    if (stationsList.isEmpty) {
-      return const Center(child: Text('No charging stations available.'));
-    }
-
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Stations',
-            style: body32MediumTextStyle,
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: stationsList.length,
-              itemBuilder: (context, index) {
-                final item = stationsList[index];
-                return Card(
-                  child: ListTile(
-                    title: Text(item.name),
-                    subtitle: Text(item.address),
-                    onTap: () {
-                      onStationSelected(item); // Call the callback on tap
-                    },
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+    return ListView.builder(
+      itemCount: stationsList.length,
+      itemBuilder: (context, index) {
+        final station = stationsList[index];
+        return ListTile(
+          title: Text(station.name),
+          subtitle: Text(station.address),
+          onTap: () => onStationSelected(station),
+        );
+      },
     );
   }
 }
