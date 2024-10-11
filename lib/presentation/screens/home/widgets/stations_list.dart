ca6326c2b8be8f44ea3c358e936fc1f12654e579
Jora <nersisyanjor3@gamil.com>
2024-10-11 13:27:22 +0400

    Changed the syntax of some Padding and Expanded.

--- lib/presentation/screens/home/widgets/stations_list.dart
+++ lib/presentation/screens/home/widgets/stations_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:watt_hub/domain/models/charging_station/charging_station_model.dart';
+import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
 class StationsList extends StatelessWidget {
   final List<ChargingStationModel> stationsList;
@@ -17,76 +18,73 @@ class StationsList extends StatelessWidget {
       itemCount: stationsList.length,
       itemBuilder: (context, index) {
         final station = stationsList[index];
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Card(
-            elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: InkWell(
-              onTap: () => onStationSelected(station),
-              borderRadius: BorderRadius.circular(15),
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      station.name,
-                      style: const TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+        return Card(
+          elevation: 4,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: InkWell(
+            onTap: () => onStationSelected(station),
+            borderRadius: BorderRadius.circular(15),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    station.name,
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const SizedBox(height: 8.0),
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.location_on,
-                          color: Colors.grey,
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.location_on,
+                        color: Colors.grey,
+                      ),
+                      const SizedBox(width: 4.0),
+                      Expanded(
+                        child: Text(
+                          station.address,
+                          style: const TextStyle(color: Colors.grey),
                         ),
-                        const SizedBox(width: 4.0),
-                        Expanded(
-                          child: Text(
-                            station.address,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.electric_car, color: Colors.green),
+                          const SizedBox(width: 4.0),
+                          Text(
+                            '${station.kwt} kW',
                             style: const TextStyle(color: Colors.grey),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 8.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            const Icon(Icons.electric_car, color: Colors.green),
-                            const SizedBox(width: 4.0),
-                            Text(
-                              '${station.kwt} kW',
-                              style: const TextStyle(color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            const Icon(Icons.attach_money, color: Colors.grey),
-                            const SizedBox(width: 4.0),
-                            Text(
-                              '\$${station.hourlyRate}/hr',
-                              style: const TextStyle(color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(Icons.attach_money, color: Colors.grey),
+                          const SizedBox(width: 4.0),
+                          Text(
+                            '\$${station.hourlyRate}/hr',
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
-        );
+        ).paddingSymmetric(horizontal: 16.0, vertical: 8.0);
       },
     );
   }
