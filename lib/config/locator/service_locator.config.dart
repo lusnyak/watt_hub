ed576f9e57a94d7177c3fc41c6da607122028f68
Jora <nersisyanjor3@gamil.com>
2024-10-11 16:12:58 +0400

    Added freezed to the bloc.

--- lib/config/locator/service_locator.config.dart
+++ lib/config/locator/service_locator.config.dart
@@ -10,8 +10,8 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
-import 'package:watt_hub/presentation/screens/home/charging_station_bloc/charging_station_bloc.dart'
-    as _i568;
+import 'package:watt_hub/presentation/screens/home/bloc/home_bloc.dart'
+    as _i944;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -24,7 +24,7 @@ extension GetItInjectableX on _i174.GetIt {
       environment,
       environmentFilter,
     );
-    gh.factory<_i568.ChargingStationBloc>(() => _i568.ChargingStationBloc());
+    gh.factory<_i944.HomeBloc>(() => _i944.HomeBloc());
     return this;
   }
 }
--- lib/presentation/screens/home/widgets/stations_list.dart
+++ lib/presentation/screens/home/widgets/stations_list.dart
@@ -21,68 +21,62 @@ class StationsList extends StatelessWidget {
         return Card(
           elevation: 4,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
+            borderRadius: BorderRadius.circular(15.r),
           ),
           child: InkWell(
             onTap: () => onStationSelected(station),
-            borderRadius: BorderRadius.circular(15),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    station.name,
-                    style: const TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
+            borderRadius: BorderRadius.circular(15.r),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  station.name,
+                  style: body18SemiBoldTextStyle,
+                ),
+                8.h.heightBox,
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.location_on,
+                      color: Colors.grey,
                     ),
-                  ),
-                  const SizedBox(height: 8.0),
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.location_on,
-                        color: Colors.grey,
+                    4.w.widthBox,
+                    Expanded(
+                      child: Text(
+                        station.address,
+                        style: const TextStyle(color: Colors.grey),
                       ),
-                      const SizedBox(width: 4.0),
-                      Expanded(
-                        child: Text(
-                          station.address,
+                    ),
+                  ],
+                ),
+                8.h.heightBox,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(Icons.electric_car, color: Colors.green),
+                        4.w.widthBox,
+                        Text(
+                          '${station.kwt} kW',
                           style: const TextStyle(color: Colors.grey),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(Icons.electric_car, color: Colors.green),
-                          const SizedBox(width: 4.0),
-                          Text(
-                            '${station.kwt} kW',
-                            style: const TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const Icon(Icons.attach_money, color: Colors.grey),
-                          const SizedBox(width: 4.0),
-                          Text(
-                            '\$${station.hourlyRate}/hr',
-                            style: const TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.attach_money, color: Colors.grey),
+                        4.w.widthBox,
+                        Text(
+                          '\$${station.hourlyRate}/hr',
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ).paddingAll(16.r),
           ),
         ).paddingSymmetric(horizontal: 16.0, vertical: 8.0);
       },
