cfb3ccd29bb7d6b5d56a9949f34eb16da238d2cf
Lusine Khachatryan <luskhach3108@gmail.com>
2024-10-11 18:01:28 +0400

    review and add todoes

---
+++ lib/data/fake_data/stations_map.dart
@@ -0,0 +1,17 @@
+Map<String, dynamic> stationJson = {
+  "id": 0,
+  "name": "",
+  "addres": "",
+  "latitude": 32.34334
+};
+
+
+List<Map<String, dynamic>> stationsListJson = [
+  {
+    "id": 0,
+    "name": "",
+    "addres": "",
+    "latitude": 32.34334,
+    "connection_type": [{}],
+  },{},
+];
