f2e2d9836a2298b293a832bde6f93dc93b75765e
Lusine Khachatryan <luskhach3108@gmail.com>
2024-10-11 18:01:28 +0400

    review and add todoes

--- lib/presentation/screens/home/widgets/station_info_modal.dart
+++ lib/presentation/screens/home/widgets/station_info_modal.dart
@@ -9,67 +9,65 @@ void showStationInfo(BuildContext context, ChargingStationModel station) {
   showModalBottomSheet(
     context: context,
     builder: (BuildContext context) {
-      return SizedBox(
-        height: 220.h,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  station.name,
-                  style: body16SemiBoldTextStyle,
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                station.name,
+                style: body16SemiBoldTextStyle,
+              ),
+              Text(
+                station.address,
+                style: body14RegularTextStyle,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                '${station.averageRate}',
+                style: body14RegularTextStyle,
+              ),
+              10.w.widthBox,
+              RatingBarIndicator(
+                rating: station.averageRate,
+                itemBuilder: (context, index) => const Icon(
+                  Icons.star,
+                  color: Colors.amber,
                 ),
-                Text(
-                  station.address,
-                  style: body14RegularTextStyle,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  '${station.averageRate}',
-                  style: body14RegularTextStyle,
-                ),
-                10.w.widthBox,
-                RatingBarIndicator(
-                  rating: station.averageRate,
-                  itemBuilder: (context, index) => const Icon(
-                    Icons.star,
-                    color: Colors.amber,
-                  ),
-                  itemCount: 5,
-                  itemSize: 25.r,
-                  direction: Axis.horizontal,
-                ),
-                10.w.widthBox,
-                Text(
-                  '(${station.reviews.length} reviews)',
-                  style: body14RegularTextStyle.copyWith(color: Colors.grey),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                WHOutlinedButton(
-                  title: 'View',
-                  onPressed: () =>
-                      AutoRouter.of(context).push(const StationInfoRoute()),
-                ).expanded(),
-                10.w.widthBox,
-                WHElevatedButton.primary(
-                  title: 'Book',
-                  onPressed: () =>
-                      AutoRouter.of(context).push(const DetailRoute()),
-                ).expanded(),
-              ],
-            ),
-          ],
-        ),
+                itemCount: 5,
+                itemSize: 25.r,
+                direction: Axis.horizontal,
+              ),
+              10.w.widthBox,
+              Text(
+                '(${station.reviews.length} reviews)',
+                style: body14RegularTextStyle.copyWith(color: Colors.grey),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              WHOutlinedButton(
+                title: 'View',
+                onPressed: () =>
+                    AutoRouter.of(context).push(const StationInfoRoute()),
+              ).expanded(),
+              10.w.widthBox,
+              WHElevatedButton.primary(
+                title: 'Book',
+                onPressed: () =>
+                    AutoRouter.of(context).push(const DetailRoute()),
+              ).expanded(),
+            ],
+          ),
+        ],
       ).paddingAll(20.r);
     },
   );
