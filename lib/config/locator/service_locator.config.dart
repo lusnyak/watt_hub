d19badd8b986ab52b06dde7af5cf0ddb817c5929
Lusine Khachatryan <luskhach3108@gmail.com>
2024-10-16 16:20:05 +0400

    fix conflict 4

--- lib/config/locator/service_locator.config.dart
+++ lib/config/locator/service_locator.config.dart
@@ -12,6 +12,8 @@ import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 import 'package:watt_hub/presentation/screens/onboarding/bloc/onboarding_bloc.dart'
     as _i764;
+import 'package:watt_hub/presentation/screens/sign_up/bloc/sign_up_bloc.dart'
+    as _i141;
 
 extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
@@ -24,6 +26,7 @@ extension GetItInjectableX on _i174.GetIt {
       environment,
       environmentFilter,
     );
+    gh.factory<_i141.SignUpBloc>(() => _i141.SignUpBloc());
     gh.factory<_i764.OnboardingBloc>(() => _i764.OnboardingBloc());
     return this;
   }
