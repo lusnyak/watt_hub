a9f37c237a74ccbec68939804dcd28f2fadf9aae
AnnaMinasyan9 <nune.minasyan.1803@gmail.com>
2024-10-17 14:45:05 +0400

    Added a basic simple version of the popup
    Added a basic simple version of the popup

--- packages/watt_hub_uikit/lib/src/widgets/widgets.dart
+++ packages/watt_hub_uikit/lib/src/widgets/widgets.dart
@@ -1,6 +1,7 @@
 export 'buttons/buttons.dart';
 export 'text_fields/text_fields.dart';
 export 'drop_downs/drop_downs.dart';
+
 export 'circular_spin/circular_spin.dart';
 export 'slider/sliders.dart';
 export 'popup/popup.dart';
