7135a4bfae06e0c2eba029766ed9dbf7903ac5fb
AnnaMinasyan9 <nune.minasyan.1803@gmail.com>
2024-10-18 15:04:11 +0400

    Some changes to the popup
    Some changes to the popup

--- lib/presentation/screens/uikit_example/uikit_popup.dart
+++ lib/presentation/screens/uikit_example/uikit_popup.dart
@@ -6,20 +6,25 @@ class UikitPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child:
-        Column(
-          children: [
-            ElevatedButton(onPressed:()=> showDialog<void>(
-        context: context,
-        builder: (BuildContext context) { return const WHPopup();}),
-               child: const Text("data"),
-
-            ),
-          ],
-        )
-      ),
+          child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () => showDialog<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return WHPopup(
+                    image: WattHubAssets.images.popupLocationImg.keyName,
+                    title: "Enable Location",
+                    subTitle:
+                        "We need access to you location to find EV charging station around you.",
+                  );
+                }),
+            child: const Text("data"),
+          ),
+        ],
+      )),
     );
   }
 }
