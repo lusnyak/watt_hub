d7115b03e0fb24d8ab99efd132889eb3a2d8a962
AnnaMinasyan9 <nune.minasyan.1803@gmail.com>
2024-10-21 12:00:45 +0400

    Added several types of popup
    Added several types of popup

--- lib/presentation/screens/uikit_example/uikit_popup.dart
+++ lib/presentation/screens/uikit_example/uikit_popup.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
-class UikitPopup extends StatelessWidget {
+class UikitPopup extends StatefulWidget {
   const UikitPopup({super.key});
+
+  @override
+  State<UikitPopup> createState() => _UikitPopupState();
+}
+
+class _UikitPopupState extends State<UikitPopup> {
+  String title = "Enable Location";
+
+  String subTitle =
+      "We need access to you location to find EV charging station around you.";
 
   @override
   Widget build(BuildContext context) {
@@ -16,15 +26,43 @@ class UikitPopup extends StatelessWidget {
                 builder: (BuildContext context) {
                   return WHPopup(
                     image: WattHubAssets.images.popupLocationImg.keyName,
-                    title: "Enable Location",
-                    subTitle:
-                        "We need access to you location to find EV charging station around you.",
+                    title: title,
+                    subTitle: subTitle,
                   );
                 }),
-            child: const Text("data"),
+            child: const Text("loading"),
+          ),
+          ElevatedButton(
+            onPressed: () => showDialog<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return WHPopup(
+                    image: WattHubAssets.images.popupLocationImg.keyName,
+                    title: "Location",
+                    subTitle: subTitle,
+                    cancelButtonTitle: "cansle",
+                    confirmButtonTitle: "save",
+                  );
+                }),
+            child: const Text("primary"),
+          ),
+          ElevatedButton(
+            onPressed: () => showDialog<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return WHPopup(
+                    image: WattHubAssets.images.popupSuccessfulImg.keyName,
+                    title: title,
+                    subTitle: subTitle,
+                    confirmButtonTitle: "ok",
+                  );
+                }),
+            child: const Text("confirm"),
           ),
         ],
       )),
     );
   }
 }
+/*
+                      */
