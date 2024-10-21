dde04ebf1b6dde38901bee2073d4a33376517f13
Vladimir Baghdasaryan <vladimirbaghdasaryan7@gmail.com>
2024-10-21 16:15:05 +0400

    added image multiple upload from camera and galery

--- lib/presentation/screens/add_station/add_station_screen.dart
+++ lib/presentation/screens/add_station/add_station_screen.dart
@@ -1,20 +1,22 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:watt_hub/config/routes/app_router.dart';
-import 'package:watt_hub/presentation/screens/add_station/bloc/add_station_bloc.dart';
-import 'package:watt_hub/presentation/screens/add_station/widgets/add_station_preview_images.dart';
-import 'package:watt_hub/presentation/screens/add_station/widgets/image_picker_upload_button.dart';
-import 'package:watt_hub/utils/extensions/extensions.dart';
-import 'package:watt_hub_localization/watt_hub_localization.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../config/locator/service_locator.dart';
-import '../../../utils/helpers/data_helper.dart';
-import '../../../utils/helpers/time_helper_format.dart';
 
 @RoutePage()
 class AddStationScreen extends StatelessWidget {
   const AddStationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) =>
+            getIt<AddStationBlock>()..add(const AddStationEvent.started()),
+        child: const AddStationView());
+  }
+}
+class AddStationView extends StatelessWidget {
+  const AddStationView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -87,106 +89,40 @@ class AddStationView extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-        child: BlocBuilder<AddStationBlock, AddStationState>(
-          builder: (context, state) {
-            return state.when(
-              initial: () => Container(),
-              loading: () => const Center(child: WHCircularSpin()),
-              error: (message) =>
-                  Center(child: Text('${context.localized.error}: $message')),
-              loaded: (connectors, initialSelectedConnectorId, images,
-                  startTime, endTime, address) {
-                final currentImages = images ?? [];
-                final initialSelectedConnector = findById(connectors,
-                    initialSelectedConnectorId, (connector) => connector.id);
-                return SingleChildScrollView(
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                        minHeight: MediaQuery.of(context).size.height),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        WHElevatedButton.secondary(
-                          title: AppLocalizations.of(context).chooseAddress,
-                        ),
-                        Text(AppLocalizations.of(context).timePicker,
-                            style: body18SemiBoldTextStyle),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            WHElevatedButton.secondary(
-                                title: AppLocalizations.of(context).startTime),
-                            WHElevatedButton.secondary(
-                                title: AppLocalizations.of(context).endTime),
-                          ],
-                        ),
-                        WHTextField.singleLine(
-                          controller: context
-                              .read<AddStationBlock>()
-                              .hourlyRateController,
-                          keyboardType: TextInputType.number,
-                          label: AppLocalizations.of(context).hourlyRate,
-                          hintText: AppLocalizations.of(context).hourlyRate,
-                        ),
-                        WHTextField.singleLine(
-                          controller: context
-                              .read<AddStationBlock>()
-                              .kilowattController,
-                          keyboardType: TextInputType.number,
-                          label: AppLocalizations.of(context).kilowatt,
-                          hintText: AppLocalizations.of(context).kilowatt,
-                        ),
-                        Text(AppLocalizations.of(context).contactInfo,
-                            style: body18SemiBoldTextStyle),
-                        WHTextField.singleLine(
-                          controller:
-                              context.read<AddStationBlock>().phoneController,
-                          keyboardType: TextInputType.phone,
-                          label: AppLocalizations.of(context).phone,
-                          hintText:
-                              AppLocalizations.of(context).yourPhoneNumber,
-                        ),
-                        WHTextField.singleLine(
-                          controller:
-                              context.read<AddStationBlock>().nameController,
-                          keyboardType: TextInputType.name,
-                          label: AppLocalizations.of(context).name,
-                          hintText: AppLocalizations.of(context).yourName,
-                        ),
-                        Text(AppLocalizations.of(context).stationImage,
-                            style: body18SemiBoldTextStyle),
-                        SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Container(
-
-                                width: MediaQuery.of(context).size.width * 0.9,
-                                height: 120.0,
-                                child: Row(children: [
-                                  if (currentImages.length < 5)
-                                    WHImagePicker.multiple(
-                                      limit: 5 - currentImages.length,
-                                      onPicked: (file) {
-                                        context.read<AddStationBlock>().add(
-                                            AddStationEvent.imagesSelected(
-                                                file));
-                                      },
-                                      child: const ImagePickerUploadButton(),
-                                    ),
-                                  Expanded(child: _previewImages(context)),
-                                ]))),
-                        WHElevatedButton.primary(
-                          title: AppLocalizations.of(context).addStation,
-                        ),
-                      ],
-                    ),
-
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            WHElevatedButton.secondary(title: "Choose an address"),
+            Text("Time Picker", style: body16SemiBoldTextStyle),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                WHElevatedButton.primary(title: "Start time"),
+                WHElevatedButton.primary(title: "End Time")
+              ],
+            ),
+            20.h.heightBox,
+            ElevatedButton(
+                onPressed: () => AutoRouter.of(context)
+                    .push(const ChooseStationAddressRoute()),
+                child: const Text(
+                  "Choose Station Address",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.blue,
                   ),
-                );
-              },
-            );
-          },
+                ))
+            WHTextField.singleLine(
+                label: "Hourly rate", hintText: "Hourly rate"),
+            WHTextField.singleLine(label: "Kilowatt", hintText: "Kilowatt"),
+            Text("Contact info", style: body16SemiBoldTextStyle),
+            // WhDropDownButton(items: items, itemLabel: itemLabel)
+          ],
         ),
       ).paddingAll(20.0),
     );
   }
 }
+
+
+
--- lib/presentation/screens/add_station/block/add_station_event.dart
+++ lib/presentation/screens/add_station/block/add_station_event.dart
@@ -1,6 +1,10 @@
-part of 'add_station_block.dart';
+part of 'add_station_bloc.dart';
 
 @freezed
 class AddStationEvent with _$AddStationEvent {
-  const factory AddStationEvent.started() = _Started;
+  const factory AddStationEvent.started() = _StartedEvent;
+  // const factory AddStationEvent.imagesSelected(List<XFile> images) = _ImagesSelectedEvent;
+
+// const factory AddStationEvent.stationConnectorTypeChanged(
+  //     ConnectorTypeModel? selectedConnector) = _StationConnectorTypeChangedEvent;
 }
