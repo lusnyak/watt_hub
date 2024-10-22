0e16f18d5eeeddd91df0aa543a35df2d801bebdb
Lusine Khachatryan <luskhach3108@gmail.com>
2024-10-22 15:55:47 +0400

    fix conflicts

--- lib/presentation/screens/uikit_example/uikit_popup.dart
+++ lib/presentation/screens/uikit_example/uikit_popup.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
-
 class UikitPopup extends StatefulWidget {
   const UikitPopup({super.key});
 
@@ -25,54 +24,53 @@ class _UikitPopupState extends State<UikitPopup> {
     return Scaffold(
       body: SafeArea(
           child: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              WHPopup.of(context).show(
-                image: WattHubAssets.images.popupSuccessfulImg.keyName,
-                title: "Verification Successful!",
-                subTitle:
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  WHPopup.of(context).show(
+                    image: WattHubAssets.images.popupSuccessfulImg.keyName,
+                    title: "Verification Successful!",
+                    subTitle:
                     "Please wait...\nYou will be directed to the homepage.",
-              );
-            },
-            child: const Text("loading"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              WHPopup.of(context).show(
-                image: WattHubAssets.images.popupLocationImg.keyName,
-                title: title,
-                subTitle: subTitle,
-                cancelButtonTitle: "Cancel",
-                confirmButtonTitle: "Enable Location",
-                onConfirmPressed: () {
-                  Navigator.pop(context);
+                  );
                 },
-                onCancelPressed: () {
-                  Navigator.pop(context);
+                child: const Text("loading"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  WHPopup.of(context).show(
+                    image: WattHubAssets.images.popupLocationImg.keyName,
+                    title: title,
+                    subTitle: subTitle,
+                    cancelButtonTitle: "Cancel",
+                    confirmButtonTitle: "Enable Location",
+                    onConfirmPressed: () {
+                      Navigator.pop(context);
+                    },
+                    onCancelPressed: () {
+                      Navigator.pop(context);
+                    },
+                  );
                 },
-              );
-            },
-            child: const Text("primary"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              WHPopup.of(context).show(
-                image: WattHubAssets.images.popupSuccessfulImg.keyName,
-                title: "Booking Successful!",
-                subTitle:
+                child: const Text("primary"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  WHPopup.of(context).show(
+                    image: WattHubAssets.images.popupSuccessfulImg.keyName,
+                    title: "Booking Successful!",
+                    subTitle:
                     "You can view booking details on the \n\t\t My Booking menu.",
-                confirmButtonTitle: "Ok",
-                onConfirmPressed: () {
-                  Navigator.pop(context);
+                    confirmButtonTitle: "Ok",
+                    onConfirmPressed: () {
+                      Navigator.pop(context);
+                    },
+                  );
                 },
-              );
-            },
-            child: const Text("confirm"),
-          ),
-        ],
-      )),
+                child: const Text("confirm"),
+              ),
+            ],
+          )),
     );
   }
 }
-
