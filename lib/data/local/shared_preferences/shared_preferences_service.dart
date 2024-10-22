cf1ebe9a1bf5f332f625a9028fad01b144f2ad80
Lusine Khachatryan <luskhach3108@gmail.com>
2024-10-22 15:34:05 +0400

    fix shared preferences

--- lib/data/local/shared_preferences/shared_preferences_service.dart
+++ lib/data/local/shared_preferences/shared_preferences_service.dart
@@ -2,10 +2,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesService {
   static const String onBoardingLaunchKey = "onBoardingLaunch";
-  static const String selectedFilterConnectorIdKey =
-      "selectedFilterConnectorIdKey";
-  static const String selectedFilterCarIdKey = "selectedFilterCarIdKey";
-  static const String filterRatingKey = "filterRatingKey";
 
   SharedPreferencesService._();
 
@@ -23,31 +19,4 @@ class SharedPreferencesService {
 
   setOnBoardingLaunch(bool value) async => await _sharedPrefs?.setBool(
       SharedPreferencesService.onBoardingLaunchKey, value);
-
-  int? selectedFilterConnectorId() => _sharedPrefs
-      ?.getInt(SharedPreferencesService.selectedFilterConnectorIdKey);
-
-  setSelectedFilterConnectorId(int value) async => await _sharedPrefs?.setInt(
-      SharedPreferencesService.selectedFilterConnectorIdKey, value);
-
-  int? selectedFilterCarId() =>
-      _sharedPrefs?.getInt(SharedPreferencesService.selectedFilterCarIdKey);
-
-  setSelectedFilterCarId(int value) async => await _sharedPrefs?.setInt(
-      SharedPreferencesService.selectedFilterCarIdKey, value);
-
-  double? filterRating() =>
-      _sharedPrefs?.getDouble(SharedPreferencesService.filterRatingKey);
-
-  setFilterRating(double value) async => await _sharedPrefs?.setDouble(
-      SharedPreferencesService.filterRatingKey, value);
-
-  deleteSelectedFilterConnectorId() async => await _sharedPrefs
-      ?.remove(SharedPreferencesService.selectedFilterConnectorIdKey);
-
-  deleteSelectedFilterCarId() async => await _sharedPrefs
-      ?.remove(SharedPreferencesService.selectedFilterCarIdKey);
-
-  deleteSelectedFilterRating() async =>
-      await _sharedPrefs?.remove(SharedPreferencesService.filterRatingKey);
 }
