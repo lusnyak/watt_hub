9bd15cf09c299a56f29b4eb3693ad3da3d9512ba
AnnaMinasyan9 <nune.minasyan.1803@gmail.com>
2024-10-24 15:56:30 +0400

    Added  uikit calendar
    Added  uikit calendar

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -30,6 +30,7 @@ class AppRouter extends RootStackRouter {
         ),
         // AutoRoute(page: SplashRoute.page),
         AutoRoute(page: UikitExampleRoute.page, initial: true),
+
         AutoRoute(page: OnboardingRoute.page),
         AutoRoute(page: DashboardRoute.page, children: [
           AutoRoute(page: HomeRoute.page),
