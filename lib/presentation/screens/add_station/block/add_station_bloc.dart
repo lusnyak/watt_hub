0bb6835228c84c21053e0ddebd0b316194d51958
Vladimir Baghdasaryan <vlaimirbaghdasaryan7@gmail.com>
2024-10-25 12:19:06 +0400

    image picker create in uikit

--- lib/presentation/screens/add_station/block/add_station_bloc.dart
+++ lib/presentation/screens/add_station/block/add_station_bloc.dart
@@ -4,7 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:bloc/bloc.dart';
 import 'package:injectable/injectable.dart';
-
 part 'add_station_event.dart';
 
 part 'add_station_state.dart';
@@ -28,26 +27,6 @@ class AddStationBlock extends Bloc<AddStationEvent, AddStationState> {
             emit(const AddStationState.loaded());
           } catch (e) {
             emit(AddStationState.error(e.toString()));
-          }
-        },
-        imagesSelected: (e) async {
-          final currentState = state;
-          if (currentState is _LoadedState) {
-            final updatedImages = List<File>.from(currentState.images ?? [])
-              ..addAll(e.images);
-            emit(AddStationState.loaded(images: updatedImages));
-          } else {
-            emit(AddStationState.loaded(images: e.images));
-          }
-        },
-        removeImage: (e) async {
-          if (state is _LoadedState) {
-            final loadedState = state as _LoadedState;
-            final updatedImages = List<File>.from(loadedState.images ?? []);
-            if (e.index >= 0 && e.index < updatedImages.length) {
-              updatedImages.removeAt(e.index);
-              emit(AddStationState.loaded(images: updatedImages));
-            }
           }
         },
       );
