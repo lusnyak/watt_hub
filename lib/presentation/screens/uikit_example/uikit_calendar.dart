66509c82424062a226a1e7ccf51d44eb7f99b92e
AnnaMinasyan9 <nune.minasyan.1803@gmail.com>
2024-10-25 14:42:11 +0400

    A few changes to the calendar
    A few changes to the calendar

--- lib/presentation/screens/uikit_example/uikit_calendar.dart
+++ lib/presentation/screens/uikit_example/uikit_calendar.dart
@@ -9,27 +9,22 @@ class UikitCalendar extends StatefulWidget {
 }
 
 class _UikitCalendarState extends State<UikitCalendar> {
-  DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-         WHCalendar(
-        focusedDay: _focusedDay,
-        selectedDay: _selectedDay,
-        onDaySelected: (selectedDay, focusedDay) {
-          setState(() {
-            _selectedDay = selectedDay;
-            _focusedDay = focusedDay;
-          });
-        },
-      ),
-
+          WHCalendar(
+            selectedDay: _selectedDay,
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+              });
+            },
+          ),
         ],
       ),
     );
