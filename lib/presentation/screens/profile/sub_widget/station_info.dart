47508e0591970d94b7e076c8c2ed2a12f272af95
Marieta Avetisyan <avetisyanmarieta1993@gmail.com>
2024-10-27 22:37:56 +0400

    added profile details page ui elements, created station_info widget

---
+++ lib/presentation/screens/profile/sub_widget/station_info.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:watt_hub_uikit/watt_hub_uikit.dart';
+
+class StationInfo extends StatelessWidget {
+  const StationInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(14),
+          border: Border.all(color: WattHubColors.primaryLightGreenColor)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text(
+            'Station Info',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ).paddingOnly(bottom: 10),
+          const Text('Street: Aram Khachatryan street'),
+          const Text('Max: 3.7kWt'),
+          const Text('Hourly rate: 10\$'),
+          const Text('Available hours: 16:00 - 18:00'),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const InkWell(
+                child: Icon(Icons.mode_edit_outlined),
+              ),
+              20.widthBox,
+              const InkWell(child: Icon(Icons.delete_outline_outlined)),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
