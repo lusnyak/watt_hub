3a232201a4709f15ff4c121a22cf44ec9c564163
Marieta Avetisyan <avetisyanmarieta1993@gmail.com>
2024-10-27 22:37:56 +0400

    added profile details page ui elements, created station_info widget

--- lib/presentation/screens/profile/profile_detail_screen.dart
+++ lib/presentation/screens/profile/profile_detail_screen.dart
@@ -1,9 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:watt_hub/data/local/onboarding_data/onboarding_data.dart';
-import 'package:watt_hub/presentation/screens/add_station/add_station_screen.dart';
-import 'package:watt_hub/presentation/screens/choose_station_address/choose_station_address_screen.dart';
-import 'package:watt_hub/presentation/screens/profile/sub_widget/profile_menu_item.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
 @RoutePage()
@@ -25,7 +21,27 @@ class ProfileDetailScreen extends StatelessWidget {
           const Icon(Icons.mode_edit_outlined).paddingSymmetric(horizontal: 20)
         ],
       ),
-      body: Center(child: Text("Profile Detail Screen")),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100), // Makes the image round
+              child: Image.asset(
+                WattHubAssets.images.profileImage.keyName,
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover, // Ensures the image fills the container
+              ),
+            ).alignAtCenter().paddingSymmetric(vertical: 10),
+            WHTextField.singleLine(label: "Full Name", hintText: "John Joe")
+                .paddingOnly(
+              bottom: 10,
+            ),
+            WHTextField.singleLine(
+                    label: "Phone Number", hintText: "+374 93123456")
+                .paddingOnly(bottom: 10),
+          ]).paddingSymmetric(horizontal: 24, vertical: 20),
     );
   }
 }
