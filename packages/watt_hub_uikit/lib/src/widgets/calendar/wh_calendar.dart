d9e46155f644882ceab39127f3df4dedccb5f296
AnnaMinasyan9 <nune.minasyan.1803@gmail.com>
2024-10-29 16:43:17 +0400

    Changes in todayDecoration design.
    Changes in todayDecoration design.

--- packages/watt_hub_uikit/lib/src/widgets/calendar/wh_calendar.dart
+++ packages/watt_hub_uikit/lib/src/widgets/calendar/wh_calendar.dart
@@ -18,42 +18,84 @@ class WHCalendar extends StatefulWidget {
 
 class _WHCalendarState extends State<WHCalendar> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime focusedDay = DateTime.now();
+  final firstDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    final firstDay = DateTime.now();
-    return TableCalendar(
-      focusedDay: DateTime.now(),
-      firstDay: firstDay,
-      lastDay: DateTime.utc(2030, 12, 31),
-      rangeStartDay: DateTime.now(),
-      availableCalendarFormats: const {
-        CalendarFormat.month: 'Month',
-        CalendarFormat.week: 'Week',
-      },
-      selectedDayPredicate: (day) {
-        return isSameDay(widget.selectedDay, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        widget.onDaySelected(selectedDay, focusedDay);
-      },
-      calendarFormat: _calendarFormat,
-      onFormatChanged: (format) {
-        setState(() {
-          _calendarFormat = format;
-        });
-      },
-      calendarStyle: const CalendarStyle(
-        todayDecoration: BoxDecoration(
-          color: WattHubColors.primaryGreenColor,
-          shape: BoxShape.circle,
+    return Container(
+      padding: const EdgeInsets.all(16.0), // Padding around the calendar
+      decoration: BoxDecoration(
+        color: WattHubColors.grayColor,
+        border: Border.all(
+          color: WattHubColors.lightGray.withOpacity(0.5),
+          width: 1.w,
         ),
+        // borderRadius: BorderRadius.circular(15),
+        borderRadius: BorderRadius.circular(20.r),
+      ),
+      child: TableCalendar(
+        rangeSelectionMode: RangeSelectionMode.disabled,
+        focusedDay: focusedDay,
+        firstDay: firstDay,
+        lastDay: DateTime.utc(2030, 12, 31),
+        // rangeStartDay: DateTime.now(),
+        availableCalendarFormats: const {
+          CalendarFormat.month: 'Month',
+          CalendarFormat.week: 'Week',
+        },
+        selectedDayPredicate: (day) {
+          return isSameDay(widget.selectedDay, day);
+        },
 
-        selectedDecoration: BoxDecoration(
-          color: WattHubColors.primaryGreenColor,
-          shape: BoxShape.circle,
+        onDaySelected: (selectedDay, focusedDay) {
+          widget.onDaySelected(selectedDay, focusedDay);
+          setState(() {
+            this.focusedDay = selectedDay;
+          });
+        },
+        calendarFormat: _calendarFormat,
+        onFormatChanged: (format) {
+          setState(() {
+            _calendarFormat = format;
+          });
+        },
+        calendarStyle: const CalendarStyle(
+          // isTodayHighlighted: false,
+
+          defaultDecoration: BoxDecoration(),
+          rowDecoration: BoxDecoration(),
+
+          holidayDecoration: BoxDecoration(),
+          todayDecoration: BoxDecoration(
+            color: WattHubColors.primaryLightGreenColor,
+            shape: BoxShape.circle,
+          ),
+          selectedDecoration: BoxDecoration(
+            color: WattHubColors.primaryGreenColor,
+            shape: BoxShape.circle,
+          ),
         ),
-
+        onPageChanged: (focusedDay) {
+          setState(() {
+            this.focusedDay = focusedDay;
+          });
+        },
+        headerStyle: HeaderStyle(
+          titleCentered: true,
+          titleTextStyle: body18SemiBoldTextStyle,
+          headerPadding: EdgeInsets.only(
+            bottom: 4.h,
+          ),
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: WattHubColors.lightGray.withOpacity(0.5),
+                width: 2.w,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
