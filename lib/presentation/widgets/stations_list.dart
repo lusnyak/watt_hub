7b231dfbfbe477447e911748c6b077d6c36e1e64
Lusine Khachatryan <luskhach3108@gmail.com>
2024-10-30 12:26:07 +0400

    fix

--- lib/presentation/widgets/stations_list.dart
+++ lib/presentation/widgets/stations_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:watt_hub/config/config.dart';
+
 import 'package:watt_hub/domain/models/station/station_model.dart';
 import 'package:watt_hub/presentation/screens/home/bloc/home_bloc.dart';
 import 'package:watt_hub/presentation/widgets/station_list_item.dart';
