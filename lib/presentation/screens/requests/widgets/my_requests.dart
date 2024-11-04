72a11baf663b9b318734ae18b9d07c14e61d49eb
Jora <nersisyanjor3@gamil.com>
2024-11-04 15:19:15 +0400

    Added two widgets for displaying requests.

---
+++ lib/presentation/screens/requests/widgets/my_requests.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:watt_hub_uikit/watt_hub_uikit.dart';
+
+class MyRequests extends StatefulWidget {
+  const MyRequests({super.key});
+
+  @override
+  State<MyRequests> createState() => _MyRequestsState();
+}
+
+class _MyRequestsState extends State<MyRequests> {
+  final List<Map<String, dynamic>> items = [
+    {'name': 'John', 'group': 'Friends'},
+    {'name': 'Jane', 'group': 'Family'},
+    {'name': 'Doe', 'group': 'Friends'},
+    {'name': 'Mark', 'group': 'Work'},
+    {'name': 'Lucy', 'group': 'Family'},
+    {'name': 'Anna', 'group': 'Work'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GroupedListView<Map<String, dynamic>, String>(
+      elements: items,
+      groupBy: (element) => element['group'],
+      groupSeparatorBuilder: (String groupValue) => Text(
+        groupValue,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      itemBuilder: (context, element) {
+        return ListTile(
+          title: Text(element['name']),
+        );
+      },
+      itemComparator: (item1, item2) => item1['name'].compareTo(item2['name']),
+    ).paddingSymmetric(vertical: 20.h).expanded();
+  }
+}
