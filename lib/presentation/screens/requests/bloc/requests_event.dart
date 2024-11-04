cdeb1aee19d5e85a5794a80cc102e55b66664587
Jora <nersisyanjor3@gamil.com>
2024-11-04 17:36:39 +0400

    Added request contents with fake handling.

--- lib/presentation/screens/requests/bloc/requests_event.dart
+++ lib/presentation/screens/requests/bloc/requests_event.dart
@@ -2,5 +2,6 @@ part of 'requests_bloc.dart';
 
 @freezed
 class RequestsEvent with _$RequestsEvent {
-  const factory RequestsEvent.selectRequestState(SegmentOption selectedOption) = _SelectRequestState;
+  const factory RequestsEvent.loadMyRequests(SegmentOption selectedOption) =
+      _LoadMyRequestsEvent;
 }
