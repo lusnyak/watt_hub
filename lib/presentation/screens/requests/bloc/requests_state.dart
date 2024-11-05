1f8222298f57028e84cef59e5671aa1e1b51d06c
Jora <nersisyanjor3@gamil.com>
2024-11-05 11:55:58 +0400

    General fixes.

--- lib/presentation/screens/requests/bloc/requests_state.dart
+++ lib/presentation/screens/requests/bloc/requests_state.dart
@@ -3,7 +3,7 @@ part of 'requests_bloc.dart';
 @freezed
 class RequestsState with _$RequestsState {
   const factory RequestsState.initial() = InitialState;
-  const factory RequestsState.loading() = LoadingState;
+  const factory RequestsState.loading({@Default(SegmentOption.my) SegmentOption selectedOption}) = LoadingState;
   const factory RequestsState.error(String message) = ErrorState;
   const factory RequestsState.loaded({
     @Default(SegmentOption.my) SegmentOption selectedOption,
