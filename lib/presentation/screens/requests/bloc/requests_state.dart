d69aba16301ddbdfa89cd71a98d78e80da5357bb
Jora <nersisyanjor3@gamil.com>
2024-11-05 11:17:27 +0400

    Fixing the idea of Loading.

--- lib/presentation/screens/requests/bloc/requests_state.dart
+++ lib/presentation/screens/requests/bloc/requests_state.dart
@@ -2,12 +2,12 @@ part of 'requests_bloc.dart';
 
 @freezed
 class RequestsState with _$RequestsState {
-  const factory RequestsState.initial() = _Initial;
-  const factory RequestsState.loading() = _LoadingState;
-  const factory RequestsState.error(String message) = _ErrorState;
+  const factory RequestsState.initial() = InitialState;
+  const factory RequestsState.loading() = LoadingState;
+  const factory RequestsState.error(String message) = ErrorState;
   const factory RequestsState.loaded({
     @Default(SegmentOption.my) SegmentOption selectedOption,
     @Default([]) List<OrderModel> myRequests,
     @Default([]) List<OrderModel> stationRequests,
-  }) = _LoadedState;
+  }) = LoadedState;
 }
