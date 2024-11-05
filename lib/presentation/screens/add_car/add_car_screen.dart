1c81221cdb8fab7220ecc4df0fc82e5a5571c2f2
19niko88 <19nikonikogh88@gmail.com>
2024-11-05 14:31:01 +0400

    Elements in the bloc are modified

--- lib/presentation/screens/add_car/add_car_screen.dart
+++ lib/presentation/screens/add_car/add_car_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:watt_hub/config/config.dart';
+import 'package:watt_hub/domain/models/car/car_model.dart';
 import 'package:watt_hub/domain/models/car_type/car_type_model.dart';
+import 'package:watt_hub/domain/models/connector_type/connector_type_model.dart';
 import 'package:watt_hub_localization/watt_hub_localization.dart';
 import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
-import '../../../config/config.dart';
-import '../../../domain/models/car/car_model.dart';
-import '../../../domain/models/connector_type/connector_type_model.dart';
 import 'bloc/add_car_bloc.dart';
 
 @RoutePage()
@@ -40,55 +40,71 @@ class _AddCarView extends StatelessWidget {
       ),
       body: SafeArea(
         child: Center(
-            child: Column(
-          children: [
-            40.h.heightBox,
-            BlocBuilder<AddCarBloc, AddCarState>(
-              builder: (context, state) {
-                return WhDropDownButton<CarTypeModel>(
-                  onChanged: (value) {
-                    addCarBloc.add(
-                      const AddCarEvent.loadCarTypes(),
-                    );
-                  },
-                  itemLabel: (carType) => carType.title ?? "",
-                  items: state.carTypes,
-                  hintText: AppLocalizations.of(context).carType,
-                );
-              },
-            ),
-            40.h.heightBox,
-            BlocBuilder<AddCarBloc, AddCarState>(
-              builder: (context, state) {
-                return WhDropDownButton<CarModel>(
-                  onChanged: (value) {
-                    addCarBloc.add(
-                      const AddCarEvent.loadCarModels(),
-                    );
-                  },
-                  itemLabel: (carModel) => carModel.title ?? "",
-                  items: state.carModels,
-                  hintText: AppLocalizations.of(context).carModel,
-                );
-              },
-            ),
-            40.h.heightBox,
-            BlocBuilder<AddCarBloc, AddCarState>(
-              builder: (context, state) {
-                return WhDropDownButton<ConnectorTypeModel>(
-                  onChanged: (value) {
-                    addCarBloc.add(
-                      const AddCarEvent.loadConnectors(),
-                    );
-                  },
-                  itemLabel: (connector) => connector.title ?? "",
-                  items: state.connectors,
-                  hintText: AppLocalizations.of(context).connectorType,
-                );
-              },
-            ),
-          ],
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              40.h.heightBox,
+              BlocBuilder<AddCarBloc, AddCarState>(
+                builder: (context, state) {
+                  return WhDropDownButton<CarTypeModel>(
+                    value: state.selectedCarType,
+                    // Shows selected item
+                    onChanged: (value) {
+                      if (value != null) {
+                        addCarBloc.add(
+                          AddCarEvent.selectCarType(value),
+                        );
+                      }
+                    },
+                    itemLabel: (carType) => carType.title ?? "",
+                    items: state.carTypes,
+                    hintText: AppLocalizations.of(context).carType,
+                  );
+                },
+              ),
+              40.h.heightBox,
+              BlocBuilder<AddCarBloc, AddCarState>(
+                builder: (context, state) {
+                  return WhDropDownButton<CarModel>(
+                    value: state.selectedCarModel,
+                    onChanged: (value) {
+                      if (value != null) {
+                        addCarBloc.add(
+                          AddCarEvent.selectCarModel(value),
+                        );
+                      }
+                    },
+                    itemLabel: (carModel) => carModel.title ?? "",
+                    items: state.carModels,
+                    hintText: AppLocalizations.of(context).carModel,
+                  );
+                },
+              ),
+              40.h.heightBox,
+              BlocBuilder<AddCarBloc, AddCarState>(
+                builder: (context, state) {
+                  return WhDropDownButton<ConnectorTypeModel>(
+                    value: state.selectedConnector,
+                    onChanged: (value) {
+                      if (value != null) {
+                        addCarBloc.add(
+                          AddCarEvent.selectConnector(value),
+                        );
+                      }
+                    },
+                    itemLabel: (connector) => connector.title ?? "",
+                    items: state.connectors,
+                    hintText: AppLocalizations.of(context).connectorType,
+                  );
+                },
+              ),
+              20.h.heightBox,
+              WHElevatedButton.primary(
+                  onPressed: () {}, title: AppLocalizations.of(context).select),
+            ],
+          ),
+        ).paddingAll(20.w),
       ),
     );
   }
