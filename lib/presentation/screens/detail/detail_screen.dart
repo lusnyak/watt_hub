e50de50cbd25965a55d7268417340523781e4af9
AnnaMinasyan9 <nune.minasyan.1803@gmail.com>
2024-11-06 14:57:57 +0400

    Created initial view of Enter Detail Page.
    Created initial view of Enter Detail Page.

--- lib/presentation/screens/detail/detail_screen.dart
+++ lib/presentation/screens/detail/detail_screen.dart
@@ -1,21 +1,72 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:watt_hub/data/fake_data/connectors_data/connectors_data.dart';
+import 'package:watt_hub_uikit/watt_hub_uikit.dart';
+
+import '../../../domain/models/connector_type/connector_type_model.dart';
 
 @RoutePage()
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const SafeArea(
-        child: Center(
-          child: Text(
-            "Detail Screen!",
-            style: TextStyle(
-              fontSize: 24.0,
-              color: Colors.blue,
+      appBar: AppBar(
+        title: const Text("Enter Detail"),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text("Select Date"),
+                WHCalendar(
+                  selectedDate: _selectedDay,
+                  onDaySelected: (selectedDate, focusedDate) {
+                    setState(() {
+                      _selectedDay = selectedDate;
+                    });
+                  },
+                ).paddingSymmetric(vertical: 8.w),
+
+                const Text("Select Arrival Time"),
+                WhDropDownButton<ConnectorTypeModel>(
+                  items: connectorsData.map((e) => ConnectorTypeModel.fromJson(e),).toList(),
+                  itemLabel: (connector) => connector.title ?? "",
+                  onChanged: (value) {},
+                ),
+                // const SizedBox(height: 16),
+                const Text("Select Charging Duration").paddingSymmetric(vertical: 8.w),
+                // const SizedBox(height: 8),
+                const Text("Comment").paddingSymmetric(vertical: 8.w),
+                WHTextField.multiLine(
+                  label: 'Comment',
+                  hintText: 'Comment',
+                  maxLines: 5,
+                  controller: TextEditingController(),
+                  onChanged: (value) => debugPrint(value),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter comment';
+                    }
+                    return null;
+                  },
+                ),
+                WHElevatedButton.primary(
+                  onPressed: () {},
+                  title: 'Continue',
+                ), // Remove the expanded() here
+              ],
             ),
           ),
         ),
