24cfbbd2b6810f7a65680ec632d062c645abb478
19niko88 <19nikonikogh88@gmail.com>
2024-11-07 15:51:41 +0400

    fixed errors after rebased

--- packages/watt_hub_uikit/lib/src/widgets/carousel_slider/wh_carousel_slider.dart
+++ packages/watt_hub_uikit/lib/src/widgets/carousel_slider/wh_carousel_slider.dart
@@ -83,11 +83,6 @@ class WHCarouselSliderState extends State<WHCarouselSlider> {
                 ),
               ),
               imageBuilder: (context, imageProvider) => Container(
-
-
-                color: WattHubColors.lightGrayColor,
-
-
                 decoration: BoxDecoration(
                   color: WattHubColors.lightGrayColor,
                   image: DecorationImage(
