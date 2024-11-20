a3c9c13a0c2ed809e723311193c1bd2913a82d25
Jora <nersisyanjor3@gamil.com>
2024-11-20 11:18:00 +0400

    Added initial info display logic for profile detail screen.

--- lib/presentation/screens/profile_detail/bloc/profile_detail_bloc.dart
+++ lib/presentation/screens/profile_detail/bloc/profile_detail_bloc.dart
@@ -2,9 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
-import 'package:watt_hub/data/fake_data/users_data/users_data.dart';
 import 'package:watt_hub/domain/models/user/user_model.dart';
-import 'package:watt_hub_uikit/watt_hub_uikit.dart';
 
 part 'profile_detail_event.dart';
 part 'profile_detail_state.dart';
@@ -17,20 +15,8 @@ class ProfileDetailBloc extends Bloc<ProfileDetailEvent, ProfileDetailState> {
 
   ProfileDetailBloc() : super(const _InitialState()) {
     on<LoadProfileEvent>((event, emit) async {
-      emit(const ProfileDetailState.loading());
-      try {
-        await 2.delay();
-
-        final user = UserModel.fromJson(userData);
-
-        fullNameController.text = event.initialFullName ?? '';
-        phoneController.text = event.initialPhoneNumber ?? '';
-        emit(ProfileDetailState.loaded(user));
-      } catch (e) {
-        emit(
-          ProfileDetailState.error('Failed to load profile: ${e.toString()}'),
-        );
-      }
+      fullNameController.text = event.initialFullName ?? '';
+      phoneController.text = event.initialPhoneNumber ?? '';
     });
   }
 }
