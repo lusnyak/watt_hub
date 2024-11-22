aa449d766be275154376c3fb6bf92529b3c2e397
Vladimir Baghdasaryan <vlaimirbaghdasaryan7@gmail.com>
2024-11-22 13:31:10 +0400

    order request

--- lib/data/remote/order_remote/order_remote.dart
+++ lib/data/remote/order_remote/order_remote.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:watt_hub/config/config.dart';
 import 'package:watt_hub/config/network/constants/constants.dart';
+import 'package:watt_hub/config/network/constants/end_points.dart';
+import 'package:watt_hub/domain/models/order/order_model.dart';
 
 part 'order_remote.g.dart';
 
@@ -9,4 +11,13 @@ part 'order_remote.g.dart';
 abstract class OrderRemoteApi {
   @factoryMethod
   factory OrderRemoteApi(Dio dio) = _OrderRemoteApi;
+
+  @GET(EndPoints.getUserCars)
+  Future<OrderModel?> getOrderById(@Path("id") String id);
+
+  @GET(EndPoints.getUserCars)
+  Future<List<OrderModel>?> getMyOrder();
+
+  @GET(EndPoints.getUserCars)
+  Future<List<OrderModel>?> getUserOrders(@Path("id") String id);
 }
--- lib/data/repository/order_repository.dart
+++ lib/data/repository/order_repository.dart
@@ -1,10 +1,18 @@
 import 'package:watt_hub/config/config.dart';
+import 'package:watt_hub/data/remote/order_remote/order_remote.dart';
+import 'package:watt_hub/domain/models/order/order_model.dart';
 
 @lazySingleton
 class OrderRepository {
-  // Future<List<ConnectorTypeModel>> connectors() async {
-  //   return await getIt<ConnectorRemoteApi>()
-  //       .connectorTypes()
-  //       .catchError((_) => <ConnectorTypeModel>[]);
-  // }
+  Future<OrderModel?> getOrderById(String id) async {
+    return await getIt<OrderRemoteApi>().getOrderById(id);
+  }
+
+  Future<List<OrderModel>?> getMyOrders() async {
+    return await getIt<OrderRemoteApi>().getMyOrder();
+  }
+
+  Future<List<OrderModel>?> getUserOrders(String id) async {
+    return await getIt<OrderRemoteApi>().getUserOrders(id);
+  }
 }
