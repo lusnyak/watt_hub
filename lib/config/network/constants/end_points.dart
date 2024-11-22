0df6fed6f81da99746bba56f53c9c8de1dbfe146
Vladimir Baghdasaryan <vlaimirbaghdasaryan7@gmail.com>
2024-11-22 11:35:49 +0400

    order endpoints

--- lib/config/network/constants/end_points.dart
+++ lib/config/network/constants/end_points.dart
@@ -2,6 +2,7 @@ abstract class EndPoints {
   static const String authOtp = '/auth/otp';
   static const String checkOtp = '/auth/check-otp';
   static const String usersMe = '/users/me';
+
   static const String stations = '/stations';
   static const String addStation = '/stations/';
   static const String stationsId = '/stations/{id}';
@@ -16,4 +17,12 @@ abstract class EndPoints {
   static const String getMyCar = "/car/my";
   static const String getUserCars = "/car/user/{id}";
   static const String addCar = "/car/";
+  static const String allOrder = "/order";
+  static const String orderId = "/order/{id}";
+  static const String createOrder = "/order/";
+  static const String orderMy = "/order/my";
+  static const String cancelledOrder = "/order/{id}/cancelled";
+  static const String declinedOrder = "/order/{id}/declined";
+  static const String updateOrder = "/order/{id}";
+  static const String userOrder = "/order/user/{id}";
 }
