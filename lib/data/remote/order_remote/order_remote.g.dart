02a75036b7055a1cd5d1bc704f3d070adc632785
Vladimir Baghdasaryan <vlaimirbaghdasaryan7@gmail.com>
2024-11-22 13:31:10 +0400

    order request

--- lib/data/remote/order_remote/order_remote.g.dart
+++ lib/data/remote/order_remote/order_remote.g.dart
@@ -132,6 +132,109 @@ class _OrderRemoteApi implements OrderRemoteApi {
     return _value;
   }
 
+  @override
+  Future<OrderModel?> getOrderById(String id) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<OrderModel>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/car/user/${id}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>?>(_options);
+    late OrderModel? _value;
+    try {
+      _value = _result.data == null ? null : OrderModel.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<List<OrderModel>?> getMyOrder() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<List<OrderModel>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/car/user/{id}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<List<dynamic>>(_options);
+    late List<OrderModel>? _value;
+    try {
+      _value = _result.data
+          ?.map((dynamic i) => OrderModel.fromJson(i as Map<String, dynamic>))
+          .toList();
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  @override
+  Future<List<OrderModel>?> getUserOrders(String id) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<List<OrderModel>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/car/user/${id}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<List<dynamic>>(_options);
+    late List<OrderModel>? _value;
+    try {
+      _value = _result.data
+          ?.map((dynamic i) => OrderModel.fromJson(i as Map<String, dynamic>))
+          .toList();
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
--- lib/data/repository/order_repository.dart
+++ lib/data/repository/order_repository.dart
@@ -1,9 +1,22 @@
 import 'package:watt_hub/config/config.dart';
 import 'package:watt_hub/data/remote/order_remote/order_remote.dart';
 import 'package:watt_hub/domain/body_requests_model/add_order/add_order_model.dart';
+import 'package:watt_hub/domain/models/order/order_model.dart';
 
 @lazySingleton
 class OrderRepository {
+  Future<OrderModel?> getOrderById(String id) async {
+    return await getIt<OrderRemoteApi>().getOrderById(id);
+  }
+
+  Future<List<OrderModel>?> getMyOrders() async {
+    return await getIt<OrderRemoteApi>().getMyOrder();
+  }
+
+  Future<List<OrderModel>?> getUserOrders(String id) async {
+    return await getIt<OrderRemoteApi>().getUserOrders(id);
+  }
+
   Future<bool> orderCancelled(int id) {
     return getIt<OrderRemoteApi>().orderCancelled(id).then((response) {
       return response.statusCode == 200 || response.statusCode == 204;
@@ -31,8 +44,10 @@ class OrderRepository {
   Future<bool> addOrder(AddOrderModel orderData) {
     return getIt<OrderRemoteApi>().addOrder(orderData).then((response) {
       return response.statusCode == 200 || response.statusCode == 204;
-    }).catchError((error) {
-      return false;
-    });
+    }).catchError(
+      (error) {
+        return false;
+      },
+    );
   }
 }
