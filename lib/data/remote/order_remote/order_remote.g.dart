a519f36e469f64fc1d75337e77492dca687df02e
Jora <nersisyanjor3@gamil.com>
2024-11-22 13:32:42 +0400

    Added order remote and repo codes.

--- lib/data/remote/order_remote/order_remote.g.dart
+++ lib/data/remote/order_remote/order_remote.g.dart
@@ -23,6 +23,115 @@ class _OrderRemoteApi implements OrderRemoteApi {
 
   final ParseErrorLogger? errorLogger;
 
+  @override
+  Future<dynamic> orderCancelled(int id) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<dynamic>(Options(
+      method: 'PUT',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/order/${id}/cancelled',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch(_options);
+    final _value = _result.data;
+    return _value;
+  }
+
+  @override
+  Future<dynamic> orderDeclined(int id) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<dynamic>(Options(
+      method: 'PUT',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/order/${id}/declined',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch(_options);
+    final _value = _result.data;
+    return _value;
+  }
+
+  @override
+  Future<dynamic> orderUpdate(int id) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<dynamic>(Options(
+      method: 'PUT',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/order/${id}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch(_options);
+    final _value = _result.data;
+    return _value;
+  }
+
+  @override
+  Future<dynamic> addOrder(AddOrderModel orderData) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(orderData.toJson());
+    final _options = _setStreamType<dynamic>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/order/',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch(_options);
+    final _value = _result.data;
+    return _value;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
--- lib/data/repository/order_repository.dart
+++ lib/data/repository/order_repository.dart
@@ -1,10 +1,38 @@
 import 'package:watt_hub/config/config.dart';
+import 'package:watt_hub/data/remote/order_remote/order_remote.dart';
+import 'package:watt_hub/domain/body_requests_model/add_order/add_order_model.dart';
 
 @lazySingleton
 class OrderRepository {
-  // Future<List<ConnectorTypeModel>> connectors() async {
-  //   return await getIt<ConnectorRemoteApi>()
-  //       .connectorTypes()
-  //       .catchError((_) => <ConnectorTypeModel>[]);
-  // }
+  Future<bool> orderCancelled(int id) {
+    return getIt<OrderRemoteApi>().orderCancelled(id).then((response) {
+      return response.statusCode == 200 || response.statusCode == 204;
+    }).catchError((error) {
+      return false;
+    });
+  }
+
+  Future<bool> orderDeclined(int id) {
+    return getIt<OrderRemoteApi>().orderDeclined(id).then((response) {
+      return response.statusCode == 200 || response.statusCode == 204;
+    }).catchError((error) {
+      return false;
+    });
+  }
+
+  Future<bool> orderUpdate(int id) {
+    return getIt<OrderRemoteApi>().orderUpdate(id).then((response) {
+      return response.statusCode == 200 || response.statusCode == 204;
+    }).catchError((error) {
+      return false;
+    });
+  }
+
+  Future<bool> addOrder(AddOrderModel orderData) {
+    return getIt<OrderRemoteApi>().addOrder(orderData).then((response) {
+      return response.statusCode == 200 || response.statusCode == 204;
+    }).catchError((error) {
+      return false;
+    });
+  }
 }
