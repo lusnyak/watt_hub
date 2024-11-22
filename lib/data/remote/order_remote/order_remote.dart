7e39951a5723583859bcce4c034601191938f42c
Jora <nersisyanjor3@gamil.com>
2024-11-22 13:32:42 +0400

    Added order remote and repo codes.

--- lib/data/remote/order_remote/order_remote.dart
+++ lib/data/remote/order_remote/order_remote.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:watt_hub/config/config.dart';
 import 'package:watt_hub/config/network/constants/constants.dart';
+import 'package:watt_hub/config/network/constants/end_points.dart';
+import 'package:watt_hub/domain/body_requests_model/add_order/add_order_model.dart';
 
 part 'order_remote.g.dart';
 
@@ -9,4 +11,22 @@ part 'order_remote.g.dart';
 abstract class OrderRemoteApi {
   @factoryMethod
   factory OrderRemoteApi(Dio dio) = _OrderRemoteApi;
+
+  @PUT(EndPoints.cancelledOrder)
+  Future<dynamic> orderCancelled(
+    @Path("id") int id,
+  );
+
+  @PUT(EndPoints.declinedOrder)
+  Future<dynamic> orderDeclined(
+    @Path("id") int id,
+  );
+
+  @PUT(EndPoints.updateOrder)
+  Future<dynamic> orderUpdate(
+    @Path("id") int id,
+  );
+
+  @POST(EndPoints.createOrder)
+  Future<dynamic> addOrder(@Body() AddOrderModel orderData);
 }
