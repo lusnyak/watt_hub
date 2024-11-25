422b0b5931dfd5991f38ece410992f28f0573e53
Jora <nersisyanjor3@gamil.com>
2024-11-25 12:13:57 +0400

    defining separate methods on the home page

--- lib/presentation/screens/profile/bloc/profile_bloc.dart
+++ lib/presentation/screens/profile/bloc/profile_bloc.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:watt_hub/config/config.dart';
-import 'package:watt_hub/data/fake_data/create_cars_data/created_cars_data.dart';
+// import 'package:watt_hub/data/fake_data/create_cars_data/created_cars_data.dart';
 import 'package:watt_hub/data/fake_data/stations_data/stations_map.dart';
 import 'package:watt_hub/data/fake_data/users_data/users_data.dart';
 import 'package:watt_hub/data/repository/station_repository.dart';
@@ -27,8 +27,8 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
         final List<StationModel> myStationData = stationsData
             .map((station) => StationModel.fromJson(station))
             .toList();
-        final List<CarModel> myCarData =
-            createdCarsData.map((car) => CarModel.fromJson(car)).toList();
+        // final List<CarModel> myCarData =
+        //     createdCarsData.map((car) => CarModel.fromJson(car)).toList();
         emit(ProfileState.loaded(
             userData: myUserData,
             stationsData: [] ,
